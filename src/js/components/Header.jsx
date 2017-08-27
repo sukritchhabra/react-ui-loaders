47e5f4ee25470f8fb4341336ab1c46285ca39555
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2017-08-27 16:43:29 -0400

    Initial Setup. (Webpack + React + SCSS + NPM)

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <div className="header">
+            <h1>React Template</h1>
+        </div>
+    );
+}
+
+export default Header;
