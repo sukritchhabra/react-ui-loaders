64830df7d182467c0600fa123f763cc2aaa7ab02
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2017-09-05 01:45:12 -0400

    Adding Jest + Procfile + server.js

---
+++ __tests__/sum.test.js
@@ -0,0 +1,5 @@
+import { Sum } from '../src/js/components/Header.jsx';
+
+test('Expect 1 + 3 to be 4', () => {
+    expect(Sum(1, 3)).toBe(4);
+});
--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 const Header = (props) => {
     return (
         <div className="header">
-            <h1>React Template</h1>
+            <h1>ReactTemplate</h1>
         </div>
     );
 }
 
+const Sum = (a, b) => {
+    return a + b;
+}
+
 export default Header;
+
+export { Sum };
